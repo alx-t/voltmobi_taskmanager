c60886d762588c67a6d7a30892195328252622bb
Alex L. Tesevich <atesevich@gmail.com>
2016-02-16 17:00:50 +0300

    add task model and controller

---
+++ spec/controllers/web/tasks_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe Web::TasksController, type: :controller do
+  let(:task) { create :task }
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Task to @task' do
+      expect(assigns(:task)).to be_a_new(Task)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+
+    context 'with valid attributes' do
+      let(:subject) { post :create, task: attributes_for(:task) }
+
+      it 'saves the new task in the database' do
+        expect { subject }.to change(Task, :count).by(1)
+      end
+
+      it 'redirect to show view' do
+        subject
+        expect(response).to redirect_to task_path(assigns(:task))
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:subject) { post :create, task: attributes_for(:invalid_task) }
+
+      it 'does not save the question' do
+        expect { subject }.to_not change(Task, :count)
+      end
+
+      it 're-renders new view' do
+        subject
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, id: task }
+
+    it 'assigns the requested task to @task' do
+      expect(assigns(:task)).to eq task
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+end
+
---
+++ spec/factories/tasks.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  sequence(:task_name) { |n| "Task name #{n}" }
+  sequence(:task_description) { |n| "Task description #{n}" }
+
+  factory :task do
+    name { FactoryGirl.generate(:task_name) }
+    description { FactoryGirl.generate(:task_description) }
+  end
+
+  factory :invalid_task, class: 'Task' do
+    name nil
+    description nil
+  end
+end
+
